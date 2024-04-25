a16a039375907e7962c96798acdfa9074934c489
PedroGTN <pedrogtneves@gmail.com>
2024-04-24 21:57:50 -0300

    Contest Básico Terminado

--- Básico/Contest_1/Pedro/D.cpp
+++ Básico/Contest_1/Pedro/D.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 int main(){
-    int k, n, w;
+    uint64_t k, w;
+    int64_t n;
     cin>>k>>n>>w;
 
-    vector<int> arr(w);
+    vector<uint64_t> arr(w);
     iota(arr.begin(), arr.end(), 1);
 
     n-=2*n;
 
-    for(auto i : arr) 
+    for(auto i : arr){
         n+=k*i;
-    
-    cout<<n<<endl;
+    }
+
+    cout<<(n<0?0:n)<<endl;
 }
